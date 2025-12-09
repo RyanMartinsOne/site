c82510de9bb9e6e883e74f9367629b288a12cbfd
Ryan Martins <ryanbarroso2017@gmail.com>
2025-12-09 15:31:55 -0300

    Add render_template

---
+++ flaskr/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('library/index.html')
+
+@app.route("/about")
+def about():
+    return "<p>About Page</p>"
